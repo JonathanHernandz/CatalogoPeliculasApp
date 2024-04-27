afff3614d0a4257ea65ec4976b467f4b172e0671
Jonathan <electroc_jonathan@hotmail.com>
2024-04-27 05:28:36 -0600

    first commit

---
+++ src/servicio/ServicioPeliculasArchivo.java
@@ -0,0 +1,115 @@
+package servicio;
+
+import dominio.Pelicula;
+
+import java.io.*;
+
+public class ServicioPeliculasArchivo implements IServicioPeliculas {
+
+    private final String NOMBRE_ARCHIVO = "peliculas.txt";
+
+    public ServicioPeliculasArchivo(){
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{
+            // Si ya existe el archivo, no se vuelve a crear
+
+            if(archivo.exists()) System.out.println("El archivo existe");
+            else {
+                // Si no existe se crea vacio
+                var salida = new PrintWriter(new FileWriter(archivo));
+                salida.close();
+                System.out.println("Se ha creado el archivo");
+            }
+
+        }catch (Exception e){
+            System.out.println("Error: "+e );
+        }
+    }
+
+
+    @Override
+    public void listarPeliculas() {
+        // Volvemos abrir el archivo
+        var archivo = new File(NOMBRE_ARCHIVO);
+        try{
+            System.out.println("Listado de peliculas");
+            // Abrimos el archivo para lectura
+            var entrada = new BufferedReader(new FileReader(archivo));
+
+            // Leemos lineaxlinea el archivo
+            String linea;
+            linea = entrada.readLine();
+
+            // Leemos todas las lineas
+            while (linea != null ){
+                var pelicula = new Pelicula(linea);
+                System.out.println(pelicula);
+                // Antes de terminar el ciclo volvemos a leer la siguiente linea
+                linea = entrada.readLine();
+
+            }
+
+            // Cerrar el archivo
+            entrada.close();
+
+        }catch ( Exception e){
+            System.out.println("Ocurrio un error al leer un archivo: "+ e.getMessage());
+        }
+    }
+
+    @Override
+    public void agregarPelicula(Pelicula pelicula) {
+        boolean anexar = false;
+        var archivo  = new File(NOMBRE_ARCHIVO);
+        try{
+            // Revisamos si existe el archivo
+            anexar = archivo.exists();
+
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+
+            // Agregamos la pelicula (toString)
+            salida.println(pelicula);
+            salida.close();
+
+            System.out.println("Se agrego al archivo: "+ pelicula);
+        }catch (Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
+
+    @Override
+    public void buscarPelicula(Pelicula pelicula) {
+        var archivo = new File(NOMBRE_ARCHIVO);
+
+        try{
+            // Abrimos el archivo para lectura LineaxLinea
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String lineaTexto;
+            lineaTexto = entrada.readLine();
+            var indice = 1;
+            boolean encontrada = false;
+            var peliculaBuscar = pelicula.getNombre();
+
+            while (lineaTexto != null){
+                // Buscamos sin importar mayusculas/minusculas
+                if(peliculaBuscar != null && peliculaBuscar.equalsIgnoreCase(lineaTexto)){
+                    encontrada = true;
+                    break;
+                }
+                // Leemos la siguiente linea de la siguiente iteracion
+                lineaTexto = entrada.readLine();
+                indice++;
+            }
+
+            // Imprimimos los resultados de la busqueda
+            if(encontrada){
+                System.out.println("Pelicula " + lineaTexto + "encontrada - Linea: "+ indice);
+            }else{
+                System.out.println("No se encontro la pelicula "+ pelicula.getNombre());
+            }
+            entrada.close();
+        }catch (Exception e){
+            System.out.println("Ocurrio un error: "+ e.getMessage());
+        }
+    }
+}
